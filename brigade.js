0ba37d0b6b1c76bfde07010d15ff8916f3251edf
Evan Steitz <ejsteitz@gmail.com>
2018-01-25 23:22:49 +0000

    Real Robot Results

--- brigade.js
+++ brigade.js
@@ -34,7 +34,7 @@ events.on("exec", (e, p) => {
 
   //*****VARIABLES*****//
   dest_dir = '/mnt/brigade/share'
-  slack_color = '#'+ Math.floor(Math.random()*16777215).toString(16)
+  test_results = ""  
 
   //*****JOBS*****//
   console.log("==> Setting up jobs...")
@@ -45,10 +45,6 @@ events.on("exec", (e, p) => {
   var minio_job = new Job("minio-job", MINIO_CONTAINER)
   minio_job.storage.enabled = true
   
-  var slack_job = new Job("slack-notify", SLACK_CONTAINER)
-  slack_job.storage.enabled = false
-  console.log("==> ...jobs setup!")
-
   //*****ENVIRONMENT*****//
   console.log("==> Setting up environment...")
   robot_job.env = {
@@ -64,21 +60,13 @@ events.on("exec", (e, p) => {
     SECRET_KEY: p.secrets.minio_pass
   }
 
-  slack_job.env = {
-    SLACK_WEBHOOK: p.secrets.slack_webhook,
-    SLACK_USERNAME: "robot-brigade-bot",
-    SLACK_TITLE: "Hello World!",
-    SLACK_MESSAGE: "It's all Kubernetes from here\n ==> Event " + e.type + " caused by " + e.provider,
-    SLACK_COLOR: slack_color,
-    SLACK_CHANNEL: "robot"
-  }
   console.log("==> ...environment setup!")
   
   //*****TASKS*****//
-  console.log("Setting up tasks...")
+  console.log("==> Setting up tasks...")
   minio_job.tasks = [
     "echo Uploading files to minio client...",
-    "node src/file-upload.js",
+    //"node src/file-upload.js",
     "echo ...files uploaded to minio client!"
   ]
 
@@ -88,16 +76,10 @@ events.on("exec", (e, p) => {
     "echo ... robot test suites finished!"
   ]
 
-  slack_job.tasks = [
-    "echo Pushing slack notification...",
-    "/slack-notify",
-    "echo ...slack notification pushed!"
-  ]
-  console.log("...tasks setup!")
+  console.log("==> ...tasks setup!")
 
 
   //*****RUN JOBS*****//
-  console.log("==> Running Jobs...")
   // run Start Job, get Promise and print results
   robot_job.run().then( resultStart => {
     //debug only
@@ -106,19 +88,13 @@ events.on("exec", (e, p) => {
     console.log("==> Start Job Done")
     console.log("==> Running Push Job")*/
     minio_job.run().then(resultM => {
-       //debug only
-       /*console.log("==> Job Results")
-       console.log(resultM.toString())
-       console.log("==> Job Done")*/
+      //debug only
+      /*console.log("==> Job Results")
+      console.log(resultM.toString())
+      console.log("==> Job Done")*/
     })
-    slack_job.run().then( resultStart => {
-    //debug only
-    /*console.log("==> slack Job Results")
-    console.log(resultStart.toString())
-    console.log("==> slack Job Done")*/
-    })
+    test_results = parseResults(resultStart.toString())
   })
-  console.log("==> ...jobs ran!")
 })
 
 
@@ -127,6 +103,43 @@ events.on("error", (e) => {
     console.log("==> Event " + e.type + " caused by " + e.provider + " cause class" + e.cause + e.cause.reason)
 })
 
-events.on("after", (e) => {  
-    console.log("After event fired " + util.inspect(e, false, null) )
+events.on("after", (e, p) => {
+  //***** Variables *****//
+  slack_color = '#'+ Math.floor(Math.random()*16777215).toString(16)
+  
+  //***** JOBS *****//
+  var slack_job = new Job("slack-notify", SLACK_CONTAINER)
+  slack_job.storage.enabled = false  
+
+  //***** ENVIRONMENT *****//
+  slack_job.env = {
+    SLACK_WEBHOOK: p.secrets.slack_webhook,
+    SLACK_USERNAME: "robot-brigade-bot",
+    SLACK_TITLE: "Robot tests have been ran. Here are the results!",
+    SLACK_MESSAGE: test_results,
+    SLACK_COLOR: slack_color,
+    SLACK_CHANNEL: "robot"  
+  }
+  
+  //***** TASKS *****//
+  slack_job.tasks = [
+    "echo Pushing slack notification...",
+    "/slack-notify",
+    "echo ...slack notification pushed!"
+  ]
+ 
+  slack_job.run().then( resultStart => {
+    //debug only
+    //console.log("==> slack Job Results")
+    //console.log(resultStart.toString())
+    //console.log("==> slack Job Done")
+  })
+  console.log("After event fired, slack updated " + util.inspect(e, false, null) )
 })
+
+function parseResults(logs) {
+  console.log('Starting result parse function...')
+  var test_results = logs.split("\n").slice(-8,-6).join("\n")
+  console.log('...result parse function complete')
+  return test_results
+}
