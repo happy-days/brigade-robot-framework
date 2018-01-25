a025a58720490fa8ba8d565d00b17417f50dc57d
Evan Steitz <ejsteitz@gmail.com>
2018-01-25 20:40:10 +0000

    WIP Slack integration

--- brigade.js
+++ brigade.js
@@ -13,70 +13,91 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+//*****CONSTANTS*****//
 const IMAGE_VERSION = "3.0.2-python2"
 const IMAGE = "quay.io/charter-ctec/rfdocker:" + IMAGE_VERSION
 
 const MINIO_VERSION = "1.0"
 const MINIO_CONTAINER = "lukepatrick/minio-node:" + MINIO_VERSION
 
+const SLACK_VERSION = "latest"
+const SLACK_CONTAINER = "technosophos/slack-notify:" + SLACK_VERSION
+
 const { events, Job } = require("brigadier")
 const util = require('util')
 
 events.on("exec", (e, p) => {
 
-  // env info
-  //console.log("==> Project " + p.name + " clones the repo at " + p.repo.cloneURL)
-  //console.log("==> Event " + e.type + " caused by " + e.provider)
+  /* DEBUG env info
+  console.log("==> Project " + p.name + " clones the repo at " + p.repo.cloneURL)
+  console.log("==> Event " + e.type + " caused by " + e.provider)*/
 
-  // create job with name and container image to use
-  var robot_job = new Job("robot-job", IMAGE) // runs helm_job 
+  //*****VARIABLES*****//
+  dest_dir = '/mnt/brigade/share'
+  slack_color = '#'+ Math.floor(Math.random()*16777215).toString(16)
+
+  //*****JOBS*****//
+  console.log("==> Setting up jobs...")
+  var robot_job = new Job("robot-job", IMAGE) 
   robot_job.storage.enabled = true
+  robot_job.docker.enabled = true
   
   var minio_job = new Job("minio-job", MINIO_CONTAINER)
   minio_job.storage.enabled = true
-
-  // set up ENV
-  robot_job.env = {
-    "DVC_USER": p.secrets.dvc_user,
-    "DVC_PASS": p.secrets.dvc_pass,
-    "DVC_IPADDR": p.secrets.dvc_ipaddr,
-    "DVC_NAME": p.secrets.dvc_name
-  }
-
-  // Destination of the robot results files
-  dest_dir = '/mnt/brigade/share'
-
-  // set up minio ENV
-  minio_job.env = {
-      "FILE_PATH": dest_dir,
-      "ACCESS_KEY": "minio",
-      "SECRET_KEY": "minio123"
-  }
   
-  //set up tasks
+  var slack_job = new Job("slack-notify", SLACK_CONTAINER)
+  slack_job.storage.enabled = false
+  console.log("==> ...jobs setup!")
+
+  //*****ENVIRONMENT*****//
+  console.log("==> Setting up environment...")
+  robot_job.env = {
+    DVC_USER: p.secrets.dvc_user,
+    DVC_PASS: p.secrets.dvc_pass,
+    DVC_IPADDR: p.secrets.dvc_ipaddr,
+    DVC_NAME: p.secrets.dvc_name
+  }
+
+  minio_job.env = {
+    FILE_PATH: dest_dir,
+    ACCESS_KEY: p.secrets.minio_user,
+    SECRET_KEY: p.secrets.minio_pass
+  }
+
+  slack_job.env = {
+    SLACK_WEBHOOK: p.secrets.SLACK_WEBHOOK,
+    SLACK_USERNAME: "robot-brigade-bot",
+    SLACK_TITLE: "Hello World!",
+    SLACK_MESSAGE: "It's all Kubernetes from here\n ==> Event " + e.type + " caused by " + e.provider,
+    SLACK_COLOR: slack_color,
+    SLACK_CHANNEL: "robot"
+  }
+  console.log("==> ...environment setup!")
+  
+  //*****TASKS*****//
+  console.log("Setting up tasks...")
   minio_job.tasks = [
-      "date",
-      "node src/file-upload.js"
+    "echo Uploading files to minio client...",
+    "node src/file-upload.js",
+    "echo ...files uploaded to minio client!"
   ]
 
-  // allow docker socket
-  robot_job.docker.enabled = true
+  robot_job.tasks = [
+    "echo Running robot test suite...",
+    "robot --outputdir /mnt/brigade/share/ /src/tests/",
+    "echo ... robot test suites finished!"
+  ]
 
-  //set up tasks
-  robot_job.tasks = [] //init empty tasks
-  
-  //Tasks
-  //Run the tests in the test directory
-  robot_job.tasks.push("echo Running robot test suite...")
-  robot_job.tasks.push("robot --outputdir /mnt/brigade/share/ /src/tests/")
-  robot_job.tasks.push("ls -al /mnt/brigade/share")
+  slack_job.tasks = [
+    "echo Pushing slack notification...",
+    "/slack-notify",
+    "echo ...slack notification pushed!"
+  ]
+  console.log("...tasks setup!")
 
-  console.log("==> Set up tasks, env, Job ")
-  //debug only
-  //console.log(robot_job)
 
-  console.log("==> Running robot_job Job")
-
+  //*****RUN JOBS*****//
+  console.log("==> Running Jobs...")
   // run Start Job, get Promise and print results
   robot_job.run().then( resultStart => {
     //debug only
@@ -91,6 +112,13 @@ events.on("exec", (e, p) => {
        console.log("==> Job Done")*/
     })
   })
+  slack.run().then( resultStart => {
+    //debug only
+    /*console.log("==> slack Job Results")
+    console.log(resultStart.toString())
+    console.log("==> slack Job Done")*/
+  })
+  console.log("==> ...jobs ran!")
 })
 
 
